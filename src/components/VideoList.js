d37eaf3615b477a059b780fde7b55f65036f2feb
Duruaku Micheal <83549815+D3troit98@users.noreply.github.com>
2022-06-24 00:26:52 +0100

    Update VideoList.js

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Paper,Typography,Grid} from "@mui/material"
+import {Grid} from "@mui/material"
 import VideoItem from './VideoItem'
 const VideoList = ({videos,onVideoSelect}) => {
     console.log(videos)
